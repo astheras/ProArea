39c4fc854d9dfbb8640bfc9567a82c1a74d6f815
Samykov <astheras@gmail.com>
2020-11-19 13:02:15 +0200

    рабочее бдение

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,14 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: _routes(),
       theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue,
+        accentColor: Colors.cyan[600],
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
--- lib/pages/post-detail.dart
+++ lib/pages/post-detail.dart
@@ -9,6 +9,8 @@ class PostDetail extends StatefulWidget {
 }
 
 class _PostDetailState extends State<PostDetail> {
+  int _currentIndex = 0;
+  Map _data;
   @override
   Widget build(BuildContext context) {
     final Map arguments = ModalRoute.of(context).settings.arguments as Map;
@@ -23,15 +25,62 @@ class _PostDetailState extends State<PostDetail> {
             child: CircularProgressIndicator(),
           );
         }
-        Map data = jsonDecode(snap.data);
-        return Padding(
-          padding: EdgeInsets.all(10),
-          child: Card(
-            child: Column(
-              Text(
-                data["name"].toString(),
+        _data = jsonDecode(snap.data);
+        return Scaffold(
+          body: Padding(
+            padding: EdgeInsets.all(15),
+            child: Card(
+              elevation: 5,
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    colors: [Colors.blue[200], Colors.red],
+                    stops: [0.0, 0.7],
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.portrait),
+                      title: Text(_data["name"]),
+                      subtitle: Text(_data["username"]),
+                    ),
+                    Card(
+                      elevation: 5,
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: _info(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            // onTap: onTabTapped, // new
+            currentIndex: _currentIndex, // new
+            onTap: onTabTapped,
+            items: [
+              new BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              new BottomNavigationBarItem(
+                icon: Icon(Icons.apartment),
+                label: 'Address',
+              ),
+              new BottomNavigationBarItem(
+                icon: Icon(Icons.precision_manufacturing_outlined),
+                label: 'Company',
+              )
+            ],
           ),
         );
 /*
@@ -44,5 +93,49 @@ class _PostDetailState extends State<PostDetail> {
         );*/
       },
     );
+  }
+
+  // render info tab
+  _info() {
+    List<Widget> render;
+    if (_currentIndex == 0) {
+      render = _home();
+    } else if (_currentIndex == 1) {
+      render = _address();
+    }
+
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: render,
+      ),
+    );
+  }
+
+  _home() {
+    return [
+      Text("email:  ${_data["email"]}"),
+      Text("phone:  ${_data["phone"]}"),
+      Text("website:  ${_data["website"]}"),
+    ];
+  }
+
+  _address() {
+    Map address = _data["address"];
+    return [
+      Text("city:  ${address["city"]}"),
+      Text("street:  ${address["street"]}"),
+      Text("suite:  ${address["suite"]}"),
+      Text("zipcode:  ${address["zipcode"]}"),
+      Text("geo:  ${address["geo"]["lat"]}; ${address["geo"]["lng"]}"),
+    ];
+  }
+
+  // selected tab
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
